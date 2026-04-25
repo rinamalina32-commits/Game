994817ddd3cb29db2a2572ace2c119e15058ba3d
Марина <rinamalina32@gmail.com>
2026-04-25 20:13:55 +0300

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,13 @@
+#include "Player.h"
+
+Player::Player() {
+    // Инициализация спрайта
+}
+
+void Player::update(float dt) {
+    // Обновление позиции
+}
+
+void Player::render(sf::RenderTarget& target) {
+    target.draw(sprite);
+}
